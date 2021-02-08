8cb0a4268d48807bf9d3bd949855bd04c1c4e67c
纪皓宸 <462499571@qq.com>
2021-02-08 20:52:56 +0800

    Book class & Date class

--- src/Project1/Book.java
+++ src/Project1/Book.java
@@ -1,13 +1,29 @@
 package Project1;
 
 public class Book {
+    private static int CurrentNumber = 10001;
     private String number; //a 5-digit serial number unique to the book
     private String name;
     private boolean checkedOut; //set to true if the book has been checked out
     private Date datePublished;
 
+    public Book(String name, Date date){
+        this.number = Integer.toString(Book.CurrentNumber++);
+        this.name = name;
+        this.checkedOut = false;
+        this.datePublished = date;
+    }
+
     @Override
-    public boolean equals(Object obj){ }
+    public boolean equals(Object obj){
+        if (obj instanceof Book){
+            return this.number.equals(((Book) obj).number);
+        }
+        return false;
+    }
+
     @Override
-    public String toString() { }
+    public String toString() {
+        return "Book#" + number + "::" + name + "::" + new SimpleDateFormat("mm/dd/yyyy").format(datePublished);
+    }
 }
--- src/Project1/Date.java
+++ src/Project1/Date.java
@@ -1,11 +1,66 @@
 package Project1;
 
+import java.util.Calendar;
+import java.util.StringTokenizer;
+
 public class Date {
+    public static final int QUADRENNIAL = 4;
+    public static final int CENTENNIAL = 100;
+    public static final int QUATERCENTENNIAL = 400;
+
     private int year;
     private int month;
     private int day;
-    public Date(String date) { } //taking mm/dd/yyyy and create a Project1.Date object
-    public Date() { } //create an object with today’s date (see Calendar class)
+    public Date(String date) {
+        StringTokenizer st = new StringTokenizer(date, "/");
+        month = Integer.parseInt(st.nextToken());
+        day = Integer.parseInt(st.nextToken());
+        year = Integer.parseInt(st.nextToken());
+    } //taking mm/dd/yyyy and create a Project1.Date object
 
-    public boolean isValid() { }
+    public Date() {
+        Calendar current = Calendar.getInstance();
+        this.year = current.get(Calendar.YEAR);
+        this.month = current.get(Calendar.MONTH);
+        this.day = current.get(Calendar.DAY_OF_MONTH);
+    } //create an object with today’s date (see Calendar class)
+
+    public boolean isValid() {
+        if (year < 1900) { return false; }
+        Date present = new Date();
+        if (year > present.year
+        || (year == present.year && month > present.month)
+        || (year == present.year && month == present.month && day > present.day)){
+            return false;
+        }
+        switch (month){
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                if (day > 31) { return false; }
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                if (day > 30) { return false; }
+                break;
+            default:
+                boolean leap = false;
+                if (year % Date.QUADRENNIAL == 0){
+                    if (year % Date.CENTENNIAL == 0){
+                        if (year % Date.QUATERCENTENNIAL == 0){
+                            leap = true;
+                        }
+                    } else { leap = true; }
+                }
+                if (!leap && day > 28) { return false; }
+                if (leap && day > 29) { return false; }
+        }
+        return true;
+    }
 }
