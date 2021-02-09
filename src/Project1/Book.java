3e8ac866c57c4eec1ec43a90fe0aeaa9600fcda0
纪皓宸 <462499571@qq.com>
2021-02-09 21:10:49 +0800

    no message

--- src/Project1/Book.java
+++ src/Project1/Book.java
@@ -1,53 +1,55 @@
 package Project1;
 
 /**
-(一句简介的话描述这个class的用途).
-(几句话详细描述这个class是怎么运作的)
-@author Haochen Ji, Yichen Chen
+ * The Book class is used to represent each Book with a name, unique number, and publication date.
+ * @author Haochen Ji, Yichen Chen
 */
 public class Book {
-    private static int CurrentNumber = 10001;
+    private static int currentNumber = 10001;
     private String number; //a 5-digit serial number unique to the book
     private String name;
     private boolean checkedOut; //set to true if the book has been checked out
     private Date datePublished;
 
     /**
-    (一句简介的话描述这个Method).
-    (有需要可以在这对这个Method里的一些东西做解释，比如要对参数param解释：@param 巴拉巴拉巴拉，一个解释一行q(≧▽≦q))
+     * Create a book with the specified name and publication date.
+     * @param name The book's name
+     * @param date The book's publication date
     */
     public Book(String name, Date date){
-        this.number = Integer.toString(Book.CurrentNumber++);
+        this.number = Integer.toString(Book.currentNumber++);
         this.name = name;
         this.checkedOut = false;
         this.datePublished = date;
     }
     
     /**
-    (一句简介的话描述这个Method).
-    (有需要可以在这对这个Method里的一些东西做解释，比如要对参数param解释：@param 巴拉巴拉巴拉，一个解释一行q(≧▽≦q))
+     * Create a book with its specific serial number
+     * A temporary instance used for {@link Library#find(Book)}
+     * @param number The number of a particular book
     */
     public Book(String number){
         this.number = number;
     }
-    
-    @Override
+
     /**
-    (一句简介的话描述这个Method).
-    (有需要可以在这对这个Method里的一些东西做解释，比如要对参数param解释：@param 巴拉巴拉巴拉，一个解释一行q(≧▽≦q))
-    */
+     * Comparing book instances by comparing their number
+     * @param obj Another book for comparison
+     * @return true if the other book's number is equal to this book's number
+     */
+    @Override
     public boolean equals(Object obj){
         if (obj instanceof Book){
             return this.number.equals(((Book) obj).number);
         }
         return false;
     }
-    
-    @Override
+
     /**
-    (一句简介的话描述这个Method).
-    (有需要可以在这对这个Method里的一些东西做解释，比如要对参数param解释：@param 巴拉巴拉巴拉，一个解释一行q(≧▽≦q))
-    */
+     * Returns a formatted string includes its number, name, publication date, and its status
+     * @return the string representation of this instance
+     */
+    @Override
     public String toString() {
         return "Book#" + number +
                 "::" + name +
@@ -56,33 +58,32 @@ public class Book {
     }
 
     /**
-    (一句简介的话描述这个Method).
-    (有需要可以在这对这个Method里的一些东西做解释，比如要对参数param解释：@param 巴拉巴拉巴拉，一个解释一行q(≧▽≦q))
-    */
+     * @return Current checkedOut flag
+     */
     public boolean isCheckedOut() {
         return checkedOut;
     }
 
     /**
-    (一句简介的话描述这个Method).
-    (有需要可以在这对这个Method里的一些东西做解释，比如要对参数param解释：@param 巴拉巴拉巴拉，一个解释一行q(≧▽≦q))
-    */
+     *
+     * @param checkedOut Change checkedOut flag
+     */
     public void setCheckedOut(boolean checkedOut) {
         this.checkedOut = checkedOut;
     }
 
     /**
-    (一句简介的话描述这个Method).
-    (有需要可以在这对这个Method里的一些东西做解释，比如要对参数param解释：@param 巴拉巴拉巴拉，一个解释一行q(≧▽≦q))
-    */
+     *
+     * @return This book's publication date
+     */
     public Date getDatePublished() {
         return datePublished;
     }
 
     /**
-    (一句简介的话描述这个Method).
-    (有需要可以在这对这个Method里的一些东西做解释，比如要对参数param解释：@param 巴拉巴拉巴拉，一个解释一行q(≧▽≦q))
-    */
+     *
+     * @return This book's number
+     */
     public String getNumber() {
         return number;
     }
