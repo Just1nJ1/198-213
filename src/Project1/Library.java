37583c4a252bc25dad6b35ac5568ec40a9a54f95
xxx-vvv <61059293+xxx-vvv@users.noreply.github.com>
2021-02-09 20:30:52 +0800

    Update Library.java

--- src/Project1/Library.java
+++ src/Project1/Library.java
@@ -1,33 +1,58 @@
 package Project1;
 
+/**
+(一句简介的话描述这个class的用途).
+(几句话详细描述这个class是怎么运作的)
+@author Haochen Ji, Yichen Chen
+*/
 public class Library {
     private Book[] books; // array-based implementation of the bag data structure
     private int numBooks; // the number of books currently in the bag
 
+    /**
+    (一句简介的话描述这个Method    default constructor to create an empty bag).
+    (有需要可以在这对这个Method里的一些东西做解释，比如要对参数param解释：@param 巴拉巴拉巴拉，一个解释一行q(≧▽≦q))
+    */
     public Library() {
         books = new Book[0];
-    } //default constructor to create an empty bag
+    }
 
+    /**
+    (一句简介的话描述这个Method    helper method to find a book in the bag).
+    (有需要可以在这对这个Method里的一些东西做解释，比如要对参数param解释：@param 巴拉巴拉巴拉，一个解释一行q(≧▽≦q))
+    */
     private int find(Book book) {
         for (int i = 0; i < books.length; i ++){
             if (books[i] != null && book.equals(books[i])){ return i; }
         }
         return -1;
-    } // helper method to find a book in the bag
+    }
 
+    /**
+    (一句简介的话描述这个Method    helper method to grow the capacity by 4).
+    (有需要可以在这对这个Method里的一些东西做解释，比如要对参数param解释：@param 巴拉巴拉巴拉，一个解释一行q(≧▽≦q))
+    */
     private void grow() {
         Book[] temp = new Book[books.length + 4];
         for (int i = 0; i < books.length; i ++){
             temp[i] = books[i];
         }
         books = temp;
-    } // helper method to grow the capacity by 4
+    }
 
+    /**
+    (一句简介的话描述这个Method).
+    (有需要可以在这对这个Method里的一些东西做解释，比如要对参数param解释：@param 巴拉巴拉巴拉，一个解释一行q(≧▽≦q))
+    */
     public void add(Book book) {
         if (books.length == numBooks){ grow(); }
         books[numBooks++] = book;
     }
 
+    /**
+    (一句简介的话描述这个Method).
+    (有需要可以在这对这个Method里的一些东西做解释，比如要对参数param解释：@param 巴拉巴拉巴拉，一个解释一行q(≧▽≦q)   @return true if checking out is successful, false otherwise)
+    */
     public boolean remove(Book book) {
         int index = find(book);
         if (index == -1){ return false; }
@@ -39,14 +64,22 @@ public class Library {
         return true;
     }
 
+    /**
+    (一句简介的话描述这个Method).
+    (有需要可以在这对这个Method里的一些东西做解释，比如要对参数param解释：@param 巴拉巴拉巴拉，一个解释一行q(≧▽≦q))
+    */
     public boolean checkOut(Book book) {
         int index = find(book);
         if (index == -1){ return false; }
         if (books[index].isCheckedOut()){ return false; }
         books[index].setCheckedOut(true);
         return true;
-    } //true if checking out is successful
+    }
 
+    /**
+    (一句简介的话描述这个Method).
+    (有需要可以在这对这个Method里的一些东西做解释，比如要对参数param解释：@param 巴拉巴拉巴拉，一个解释一行q(≧▽≦q))
+    */
     public boolean returns(Book book) {
         int index = find(book);
         if (index == -1){ return false; }
@@ -55,6 +88,10 @@ public class Library {
         return true;
     }
 
+    /**
+    (一句简介的话描述这个Method    print the list of books in the bag).
+    (有需要可以在这对这个Method里的一些东西做解释，比如要对参数param解释：@param 巴拉巴拉巴拉，一个解释一行q(≧▽≦q))
+    */
     public void print() {
         System.out.println("**List of books in the library.");
         for (Book b : books){
@@ -63,8 +100,12 @@ public class Library {
             }
         }
         System.out.println("**End of list");
-    } //print the list of books in the bag
+    }
 
+    /**
+    (一句简介的话描述这个Method    print the list of books by datePublished (ascending)).
+    (有需要可以在这对这个Method里的一些东西做解释，比如要对参数param解释：@param 巴拉巴拉巴拉，一个解释一行q(≧▽≦q))
+    */
     public void printByDate() {
         for (int i = 0; i < numBooks; i++)
         {
@@ -84,8 +125,12 @@ public class Library {
             }
         }
         System.out.println("**End of list");
-    } //print the list of books by datePublished (ascending)
+    }
 
+    /**
+    (一句简介的话描述这个Method    print the list of books by number (ascending)).
+    (有需要可以在这对这个Method里的一些东西做解释，比如要对参数param解释：@param 巴拉巴拉巴拉，一个解释一行q(≧▽≦q))
+    */
     public void printByNumber() {
         for (int i = 0; i < numBooks; i++) {
             for (int j = i + 1; j < numBooks; j++) {
@@ -104,8 +149,12 @@ public class Library {
             }
         }
         System.out.println("**End of list");
-    } //print the list of books by number (ascending)
+    }
 
+    /**
+    (一句简介的话描述这个Method).
+    (有需要可以在这对这个Method里的一些东西做解释，比如要对参数param解释：@param 巴拉巴拉巴拉，一个解释一行q(≧▽≦q))
+    */
     public int getNumBooks() {
         return numBooks;
     }
