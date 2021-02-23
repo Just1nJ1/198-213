6f0f6be6057302072f419db6be26a7f60b439fe2
xxx-vvv <61059293+xxx-vvv@users.noreply.github.com>
2021-02-23 13:49:03 +0800

    Update Management.java

--- src/Project2/Management.java
+++ src/Project2/Management.java
@@ -1,19 +1,23 @@
 package Project2;
 
+/**
+ * (...)
+ * @author Haochen Ji, Yichen Chen
+ */
 public class Management extends Fulltime {
 
     @Override
     public String toString() {
         return super.toString();
-    }
+    } //(...)
 
     @Override
     public boolean equals(Object obj) {
         return super.equals(obj);
-    }
+    } //(...)
 
     @Override
     public void calculatePayment() {
         super.calculatePayment();
-    }
+    } //(...)
 }
