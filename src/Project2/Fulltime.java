96d4e5f6f81bc2e1c874af7e652528f6b2e9f300
xxx-vvv <61059293+xxx-vvv@users.noreply.github.com>
2021-02-23 13:48:28 +0800

    Update Fulltime.java

--- src/Project2/Fulltime.java
+++ src/Project2/Fulltime.java
@@ -1,19 +1,23 @@
 package Project2;
 
+/**
+ * (...)
+ * @author Haochen Ji, Yichen Chen
+ */
 public class Fulltime extends Employee {
 
     @Override
     public String toString() {
         return super.toString();
-    }
+    } //(...)
 
     @Override
     public boolean equals(Object obj) {
         return super.equals(obj);
-    }
+    } //(...)
 
     @Override
     public void calculatePayment() {
         super.calculatePayment();
-    }
+    } //(...)
 }
