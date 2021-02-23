2aaec4c1d43e77ec7fe7068f2b4ef4c47b7cb5cd
xxx-vvv <61059293+xxx-vvv@users.noreply.github.com>
2021-02-23 13:47:39 +0800

    Update Employee.java

--- src/Project2/Employee.java
+++ src/Project2/Employee.java
@@ -1,18 +1,22 @@
 package Project2;
 
+/**
+ * (...)
+ * @author Haochen Ji, Yichen Chen
+ */
 public class Employee {
 
     @Override
     public String toString() {
         return super.toString();
-    }
+    } //(...)
 
     @Override
     public boolean equals(Object obj) {
         return super.equals(obj);
-    }
+    } //(...)
 
     public void calculatePayment(){
 
-    }
+    } //(...)
 }
